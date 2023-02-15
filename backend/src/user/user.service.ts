cec88b21f38a0578819fcca34a0ca8e9bc45e08e
taekyung98 <ks87071@naver.com>
2023-02-15 16:47:08 +0900

    init

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { SignUpUserDto } from './dto/sign-up-user.dto';
 import { User, UserDocument } from './entities/user.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { throws } from 'assert';
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
     async signUp(signUpUserDto: SignUpUserDto): Promise<{ result: boolean }> {
-        //const { userName, userEmail, userPwd } = signUpUserDto;
+        const { userName, userEmail, userPwd } = signUpUserDto;
+
+        const emailRegExp = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6})*$/;
+        if (userEmail.trim().length === 0 || !emailRegExp.test(userEmail)) {
+            throw new BadRequestException('유효하지 않는 이메일 주소입니다.');
+            console.log(BadRequestException);
+        }
+
         signUpUserDto.signDate = new Date();
         const signedUser = new this.userModel(signUpUserDto);
         await signedUser.save();
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } =require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  transpileDependencies: true
 })
